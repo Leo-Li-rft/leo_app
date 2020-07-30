de3766b1ce297caa378cf718655baa7b69f6f6c0
leoli2 <leoli2@refinitiv.com>
2020-07-30 14:10:00 +0800

    aa

--- leo/main.tf
+++ leo/main.tf
@@ -2,28 +2,11 @@ provider "aws" {
   version = "~> 2.7"
   region  = "us-east-1"
 }
-data "aws_s3_bucket_object" "im_package" {
-  bucket = "a205419-evmdev-git2s3-start"
-  key    = "IM/cvm_im.jar"
-}
-resource "aws_lambda_function" "a205419-cvm-hello" {
-  s3_bucket = data.aws_s3_bucket_object.im_package.bucket
-  s3_key = data.aws_s3_bucket_object.im_package.key
-  s3_object_version = data.aws_s3_bucket_object.im_package.version_id
-  function_name = "a205419-cvm-hello"
-  role          = "arn:aws:iam::608014515287:role/a205419-vmauto-dev"
-  handler       = "com.refinitiv.cvm.HelloLambda::handleRequest"
-  runtime = "java8"
-  tags = var.default_tags
-}
 
-resource "aws_lambda_function" "a205419-cvm-hello" {
-  s3_bucket = data.aws_s3_bucket_object.im_package.bucket
-  s3_key = data.aws_s3_bucket_object.im_package.key
-  s3_object_version = data.aws_s3_bucket_object.im_package.version_id
-  function_name = "a205419-cvm-hello"
-  role          = "arn:aws:iam::608014515287:role/a205419-vmauto-dev"
-  handler       = "com.refinitiv.cvm.HelloLambda::handleRequest"
-  runtime = "java8"
-  tags = var.default_tags
+resource "aws_s3_bucket" "my_bucket" {
+  bucket  = var.bucket_name
+  acl    = "public-read"
+  tags = {
+    Name = "Leo"
+  }
 }
--- leo/variable.tf
+++ leo/variable.tf
@@ -5,6 +5,11 @@ variable "default_tags" {
         "tr:application-asset-insight-id": "205419"
         "tr:environment-type": "DEVELOPMENT"
         "tr:service-name": "VM"
-        "tr:resource-owner": "jia.ma@refinitiv.com"
+        "tr:resource-owner": "leoli2@refinitiv.com"
   }
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "a205419-cvm-ve-bucket-20200730"
 }
